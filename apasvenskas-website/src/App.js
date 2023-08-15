0a240c5796d34291830cecc0884d9dea0750cf8c
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-08-14 17:23:07 -0700

    first commit

---
+++ apasvenskas-website/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+      <h1>Audrius Pasvenskas</h1>
+      <div className='introduction'>
+        <h3>introduction</h3>
+        <p>Introductory text goes here: For example: Hello my name is Audrius and I am a software developer. My expertice and knowledge are primarly with HTML, CSS, Java Script, and React. I am also proficient with using Redux and some other tools that come with it such as Thunk.</p>
+      </div>
+    </header>
+    </div>
+  );
+}
+
+export default App;
