63c78c182f8a0c2b2922bed0896500f4f06df0ba
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-08-31 19:51:04 -0700

    slight router modification

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export function AboutMe() {
-    
+    return (
+        <h1>Where I come from</h1>
+    )
 }
--- src/components/education.js
+++ src/components/education.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export function Education() {
-    
+    return(
+        <h1>Education</h1>
+    )
 }
