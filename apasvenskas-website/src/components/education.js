241d6ea9414aeb89f34ee1dd67a91332a869b1b3
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-09-14 19:07:38 -0700

    working on experience, work in progress

--- apasvenskas-website/src/components/education.js
+++ apasvenskas-website/src/components/education.js
@@ -1,21 +1,33 @@
 import React from "react";
 
 export function Education() {
-    return(
-        <>
-        <h1>Education</h1>
+  return (
+    <>
+      <h1>Education</h1>
+      <div>
         <div>
-            <div>
-                <h3>Bachelors Degree</h3>
-                <p>
-                    I have Bachelors of Science degree from California State University Maritime Academy. I studied Maritime Transportation. Once I finished studies and passed all the requered USCG test I also received a third mate's unlimited license for all the oeceans. It is a license to navigate a vessel as a mate/officer. The studies were challenging some of the most challenging classes were Celestial Navigation, Radar, GMDSS, and Ship Stability. Nonetheless, the 7 USCG exams were the most challenging part of all the studies since they were comprehensive on entire material studied during the years, and they all had to be passed in order to graduate.  
-                </p>
-            </div>
-            <div>
-                <h3>Bloomtech</h3>
-                <p>I am currently attending Bloomtech studying full stack web development</p>
-            </div>
+          <h3>Bloomtech</h3>
+          <p>
+            I am currently attending Bloomtech studying full stack web
+            development
+          </p>
         </div>
-        </>
-    )
+        <div>
+          <h3>Bachelors Degree</h3>
+          <p>
+            I have Bachelors of Science degree from California State University
+            Maritime Academy. I studied Maritime Transportation. Once I finished
+            studies and passed all the requered USCG test I also received a
+            third mate's unlimited license for all the oeceans. It is a license
+            to navigate a vessel as a mate/officer. The studies were challenging
+            some of the most challenging classes were Celestial Navigation,
+            Radar, GMDSS, and Ship Stability. Nonetheless, the 7 USCG exams were
+            the most challenging part of all the studies since they were
+            comprehensive on entire material studied during the years, and they
+            all had to be passed in order to graduate.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
