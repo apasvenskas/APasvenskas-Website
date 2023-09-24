06c3f99dbbdbab4b37486ceccbc6b61871d99037
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-09-23 20:54:39 -0700

    added info to project and aboutme tabs

--- apasvenskas-website/src/components/projects.js
+++ apasvenskas-website/src/components/projects.js
@@ -2,7 +2,13 @@ import React from "react";
 
 export function Projects() {
     return(
+        <>
         <h1>Projects</h1>
+        <div className="AIproject">
+            <h3>AI Sumarizer</h3>
+            <p>A single page App that sumarises a webpage or an article.</p>
+            <button>AI Sumarizer</button>
+        </div>
+        </>
     )
-    
 }
