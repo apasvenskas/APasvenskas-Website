c979dcb555b8567346941ee13fa3d8de9ff3c0e8
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-01-09 00:44:49 -0800

    arrow keys working for the game

--- src/components/home.js
+++ src/components/home.js
@@ -1,43 +1,36 @@
 // home.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Button } from "@material-ui/core";
 import Game from "./Game/snakeGame";
 import "./Game/snakeGame.css";
 import NewWindow from "react-new-window";
-// import styles from './home.module.css';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showGame: false,
-    };
-  }
+function Home(props) {
+  const [showGame, setShowGame] = useState(false);
 
-  // toggle the game visibility
-  handleClick = () => {
-    this.setState((prevState) => ({
-      showGame: !prevState.showGame,
-    }));
+  const handleClick = () => {
+    setShowGame((prevState) => !prevState);
   };
 
-  render() {
-    return (
-      <div className="game">
-        <h1>Games</h1>
-        <p className="gameDescription">A simple snake game that will open in a new window for desktop browser.</p>
-        <button className="snakeGame" onClick={this.handleClick}>Play Snake Game</button>
-        {this.state.showGame && (
-          <NewWindow>
-            <Game />
-          </NewWindow>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="game">
+      <h1>Games</h1>
+      <p className="gameDescription">
+        A simple snake game that will open in a new window for desktop browser.
+      </p>
+      <button className="snakeGame" onClick={handleClick}>
+        Play Snake Game
+      </button>
+      {showGame && (
+        <a href="/Game/snakeGame" target="_blank" rel="noopener noreferrer">
+          <Game />
+        </a>
+      )}
+    </div>
+  );
 }
-
 ReactDOM.render(<Button />, document.getElementById("root"));
 
 export default Home;
+
