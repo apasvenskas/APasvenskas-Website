73e64c25236f69f30af3d0bedddd5d2f4023e119
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-01-17 22:09:19 -0800

    rewrting back end issue with node and react app

--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
-const express = require("express")
+// const express = require("express")
+// const server = express()
+// const router = require('./router.js')
+// const contactRouter = require('./components/contactsRouter.js')
+
+// server.use(express.json())
+// server.use("/website", router)
+// server.use("/contacts", contactRouter)
+
+// server.get('/', function (req, res) {
+//     res.send('Hello, this is the default page of my server.');
+//   });
+
+// module.exports = server
+const express = require("express");
 const server = express()
-const router = require('./router.js')
-const contactRouter = require('./components/contactsRouter.js')
+const HOST = 'Localhost'
+const PORT = 3003
+const testData = require('../testData')
 
 server.use(express.json())
-server.use("/website", router)
-server.use("/contacts", contactRouter)
 
-server.get('/', function (req, res) {
-    res.send('Hello, this is the default page of my server.');
-  });
+server.get('/', function (req, res){
+  res.send('Hello, this is the default page of my server.');
+})
 
-module.exports = server
+server.get('/contacts', (req, res) => {
+  // post user data
+  res.json(testData)
+})
+
+server.post('/contacts', (req, res) => {
+  // post user data
+})
+server.listen(PORT, () => console.log(`Server running at ${HOST}:${PORT}`));
