cfa4ad1601042c39d00a0c48305518158adb60b3
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-01-21 23:45:41 -0800

    updated css for contacts

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -8,7 +8,7 @@ export function AboutMe() {
             <h3>Birthplace</h3>
             <p>I was born in Kaunas, Lithuania in the year of 1991. I have lived there till the age of 12 years. At twelve I moved to United States Where my father was already residing. At the time my dad lived in Palm Springs, CA and that is were I moved to. Since than I have moved through out the state I have lived in the following cities: San Diego, Palm Springs, Vallejo, and Long Beach. As stated, do to education and work I have moved alot. In those years I have visited Lithuania many times including almost 4 years after I turend 18. As of right now I am a US citizen and residing in Palm Springs, CA together with my father.</p>
         </div>
-        <div>
+        <div className="intrest">
             <h3>Intrest in Software Development</h3>
             <p>I can remember the first day my mom brought a computer to the apartment we lived in Lithuania. At the time I was around 7 years old. Right away it was something special my previos experience with computers was limited to brief introductions to video game consoles such as SEGA and Nintendo. Eventhought, I was not alowed to touch the desktop without supervision of my mother. Over the years I ended up spending more and more time with it. At the time my mother worked as acountant in a new Lithuanian startup company that developed finiancial acounting Software.</p>
         </div>
