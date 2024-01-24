76eb81ea7c8fa92fb3a14e136b9469bd984ef443
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-01-23 19:43:18 -0800

    chnaged some text introduction and education

--- apasvenskas-website/src/components/education.js
+++ apasvenskas-website/src/components/education.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./education.css"
 
 export function Education() {
@@ -9,9 +10,9 @@ export function Education() {
         <div className="header">
           <h3>Bloomtech</h3>
           <p>
-            I am currently attending Bloomtech studying full stack web
-            development
+          I completed a Full Stack Web Development program at Bloomtech, where I learned both front end and back end web development. The program taught me HTML, CSS, React libraries, Node.js, and SQL, along with many other tools and dependencies, such as knex.js, Redux, Auth0, and more. Furthermore, a large part of the course was dedicated to Algorithms, Big-O Notation, and Data Structures. Here is a link to the certificate .  
           </p>
+          <Link to="https://app.bloomtech.com/certificate">View certificate</Link>
         </div>
         <div className="body">
           <h3>Bachelors Degree</h3>
